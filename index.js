c54e076cc3ed71d4d9b611b62ca5e424793dc589
Rodrigo Nathan <rodrigonathan4101@gmail.com>
2023-11-06 19:03:06 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,31 @@
+function calcularGorjeta(event){
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQual = document.getElementById('serviceQual').value;
+    let pessoas = document.getElementById('people').value;
+
+    if(bill == ''| serviceQual == 0 ){
+        alert("Por favor, preencha os valores")
+        return;
+    }
+
+    if(pessoas == "" | pessoas <=1 ){
+        pessoas = 1;
+        document.getElementById('each').style.display= "none"
+    } else{
+        document.getElementById('each').style.display= "block" 
+    }
+
+
+    let total = (bill*serviceQual)/pessoas;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display="block";
+
+}
+
+document.getElementById('totalTip').style.display="none";
+document.getElementById('each').style.display="none";
+
+
+document.getElementById('formulario').addEventListener('submit', calcularGorjeta);
